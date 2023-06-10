ed1db84fabccdfb62fd0d130240629a73a549498
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-06-09 19:57:23 -0700

    added more about me stuff

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,11 +5,11 @@ export default function AboutMe () {
         <>
         <div className='about-me'>
             <p>
-                My name is Darryl Tillman. I am a full-stack developer currently looking for work. I have a bachelors in the fine arts that I received in 2020 from the University of Washington. After graduation, the job market was in a tough spot post pandemic, and I found that my chosen career path was not fullfilling my needs. I decided to make a drastic change and enroll in the University of Washington Coding Bootcamp.
+                My name is Darryl Tillman. I am a full-stack developer currently looking for work. I have whent to the Art institute of Pittsburgh for animation back in 2007 and it seems to be a great way to connect with the coding for me.
             </p>
             <p>
-                I was familiar with the concept of a coding bootcamp as part of my coursework for my bachelors was doing a reasearch project working with a local organization (Skillspire) to document their work towards creating opportunities for people of color and women in the tech space. You can find the link to that research archive 
-                <a href="https://cdm16786.contentdm.oclc.org/digital/collection/p16786coll12/search/searchterm/xxskillspire" target="_blank" rel="noreferrer"> here.</a>
+                I was familiar with the concept of a coding bootcamp as part of my coursework the art institute of Pittsburgh.
+                
             </p>
         </div>
         </>
